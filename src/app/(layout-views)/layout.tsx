b26ace5a87da96e15cb75507defb72895677a22f
Yoan <yoan16908@gmail.com>
2025-10-04 15:08:44 -0400

    Added layout md

--- src/app/(layout-views)/layout.tsx
+++ src/app/(layout-views)/layout.tsx
@@ -1,9 +1,18 @@
-export default function RootLayout({
+import { Box } from "@mui/material";
+
+export default function Roout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return <>
+  <Box sx={{
+    marginX:'auto',
+    maxWidth:'md',
+    minHeight:'100dvh'
+  }}
+  >
     {children}
+  </Box>
   </>
 }
