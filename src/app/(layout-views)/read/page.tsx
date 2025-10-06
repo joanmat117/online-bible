5b414272ad37ba31b42b6b610072a0cf03cc64e8
yoan <joanmat@proton.me>
2025-10-06 19:12:01 -0400

    testing neovim on laptop

--- src/app/(layout-views)/read/page.tsx
+++ src/app/(layout-views)/read/page.tsx
@@ -1,5 +1,10 @@
 export default function Read(){
 	return <>
-	<p>Lorem	afds jfhsiu hsuighdsfjig df</p>
+	<p>Papa , tu te puedes creer que este texto lo estoy escribiendo en:</p>
+  <h2>NEOVIM</h2>
+  <ul>
+    <li>En serio esto lo estoy escribiendo desde nvim?</li>
+     <li>En serio esto lo estoy escribiendo desde nvim?</li> 
+  </ul>
 	</>
 }
