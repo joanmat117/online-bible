5be6d4ddcd08930f19128eca35f3fef6a7c9ebf3
yoan <joanmat@proton.me>
2025-10-18 15:30:29 -0400

    fix error: useSearchParams should be wrapped in a Suspense Boundary

--- src/app/(layout-views)/read/page.tsx
+++ src/app/(layout-views)/read/page.tsx
@@ -1,8 +1,11 @@
 import { ChapterRender } from "@/components/chapterRender"
+import {Suspense} from 'react'
 
 export default function Read(){
  
   return <>
+  <Suspense fallback={''}>
   <ChapterRender/>
+  </Suspense>
 	</>
 }
