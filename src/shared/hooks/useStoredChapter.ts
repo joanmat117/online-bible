1ac7f236af07aebc9a7e9b779f2be483c431b0bc
yoan <joanmat@proton.me>
2025-11-15 13:43:22 -0500

    fix(hook): fix error where localStorage was indefined in useStoredChapter

--- src/shared/hooks/useStoredChapter.ts
+++ src/shared/hooks/useStoredChapter.ts
@@ -1,19 +1,21 @@
 "use client"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CurrentChapter } from "@/shared/contexts/useBibleStore";
 
+export function useStoredChapter() {
+  const [storedChapter, setStoredChapter] = useState<CurrentChapter | null>(null);
 
-export function useStoredChapter(){
-  const [storedChapter,setStoredChapter] = useState<CurrentChapter|null>(()=>localStorage.getItem('currentChapter')? 
-    JSON.parse(localStorage.getItem('currentChapter') as string):
-    null
-   )
+  useEffect(() => {
+    const stored = localStorage.getItem('currentChapter');
+    if (stored) {
+      setStoredChapter(JSON.parse(stored));
+    }
+  }, []);
 
-   const changeStoredChapter = (chapter:CurrentChapter)=>{
-    localStorage.setItem('currentChapter',JSON.stringify(chapter))
-    setStoredChapter(chapter)
-   }
-  
+  const changeStoredChapter = (chapter: CurrentChapter) => {
+    localStorage.setItem('currentChapter', JSON.stringify(chapter));
+    setStoredChapter(chapter);
+  }
 
-  return {storedChapter,setStoredChapter:changeStoredChapter}
+  return { storedChapter, setStoredChapter: changeStoredChapter }
 }
